53839fffb5b58f4c59aefbe790f36ec97935a9da
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-05-07 10:06:16 +0430

    make first item in the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
-
+import styles from '../styles/Navbar.module.css'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+      <div className={styles.callButton}>
+        <Image src='/img/phone.png' alt='this is phone for the Restaurant' width={32} height={32}/>
+      </div>
+      <div className={styles.texts}>
+      <div className={styles.text}>ORDER NOW!</div>
+      <div className={styles.text}>012 345 678</div>
+
+      </div>
+      </div>
+      <div className={styles.item}>center</div>
+      <div className={styles.item}>right</div>
+
+    </div>
   )
 }
 
